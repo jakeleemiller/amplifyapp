87d21eaffb4a75fea2114393a52cc1d6825ff124
Jake Miller <jakeleemiller@hotmail.com>
2022-04-14 16:02:41 -0400

    Added auth

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css'; // default theme
 import { Amplify } from 'aws-amplify';
 import awsExports from './aws-exports';
-
 Amplify.configure(awsExports);
 
 function App() {
@@ -16,7 +15,7 @@ function App() {
             <header className="App-header">
               <img src={logo} className="App-logo" alt="logo" />
               <p>
-                Hello World
+                Greetings Earth
               </p>
               <a
                 className="App-link"
