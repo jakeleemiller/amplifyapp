9c2224f0e17b034b5eb67cf49205848ef1003ac8
Jake Miller <jakeleemiller@hotmail.com>
2022-04-14 14:28:10 -0400

    Authentication Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css'; // default theme
 import { Amplify } from 'aws-amplify';
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
